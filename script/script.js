2aedcdb4a8952bb70eea4dd68d81812eb98d4747
guillaume <guillaumechiron4@gmail.com>
2026-02-06 14:16:39 +0100

    modifs script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,3 @@
-const url = "http://localhost:8000/api/v1/titles/499549"
-
 async function recupererDonnees(requete) {
   try {
     const response = await fetch(requete);
@@ -11,13 +9,27 @@ async function recupererDonnees(requete) {
 }
 
 
-async function modifmeilleurFilm(requete){
-    const titrefilm = await recupererDonnees(requete)
-    
-    console.log(titrefilm)
-    document.querySelector(".element_mf_2").textContent = titrefilm.original_title
-    document.querySelector(".element_mf_3").textContent = titrefilm.long_description
-    document.querySelector(".element_mf_1").setAttribute("src", titrefilm.image_url)
+async function modifmeilleurFilm(requete) {
+  const data = await recupererDonnees(requete)
+  console.log(data)
+
+  document.querySelector(".element_mf_2").textContent = data.original_title
+  document.querySelector(".element_mf_3").textContent = data.long_description
+  let image = document.querySelector(".element_mf_1")
+  if ("image_url" in data) {
+    image.setAttribute("src", data.image_url)
+  } else {
+    console.log("l'image n'existe pas")
+  }
+
 }
 
-modifmeilleurFilm(url)
+async function test(requete) {
+  const data = await recupererDonnees(requete)
+  console.log(data)
+
+}
+
+
+
+modifmeilleurFilm("http://localhost:8000/api/v1/titles/1508669")
